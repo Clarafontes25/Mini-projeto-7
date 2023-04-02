9bf3e9e35c3316660a6806ceb8d5181903dc779e
Clarafontes25 <117857336+Clarafontes25@users.noreply.github.com>
2023-04-02 13:33:18 -0300

    Create Escolha.java

---
+++ Escolha.java
@@ -0,0 +1,11 @@
+public class Escolha {
+    public String texto;
+    public Capitulo próximo;
+    Escolha(String texto, Capitulo próximo){
+        this.texto = texto;
+        this.próximo = próximo;
+    }
+
+
+  
+}
